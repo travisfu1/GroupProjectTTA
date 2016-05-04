8cdf277959124d6d698f543495b4632358c38965
Travis FU <travis.b.fu@gmail.com>
2016-05-04 10:26:47 -0700

    began project

---
+++ groupProject/src/Zoo/Animals.java
@@ -0,0 +1,15 @@
+package Zoo;
+
+public abstract class Animals {
+	private String name;
+	
+	public Animals(String nombre){
+		name = nombre;
+	}
+		
+	public abstract String talk();
+	
+	public String toString(){
+		return name + " says " + talk(); 
+	}
+}
