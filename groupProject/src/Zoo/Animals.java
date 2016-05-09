0a05124b583b30af200470dfdfc7765312bb7f22
Travis FU <travis.b.fu@gmail.com>
2016-05-09 10:38:01 -0700

    adsfasd

--- groupProject/src/Zoo/Animals.java
+++ groupProject/src/Zoo/Animals.java
@@ -1,5 +1,37 @@
 package Zoo;
 
-public class Animals {
+public abstract class Animals {
+	
+	private int weight;
+	private String name; 
+	
+	public Animals(){
+		weight = 10; 
+		name = "Andy";
+	}
+	
+	public Animals(int w){
+		
+	}
+	
+	public void setWeight(int w){
+		weight = w; 
+	}
+	public int getWeight(){
+		return weight; 
+	}
 
+	public void setName(String n){
+		name = n; 
+	}
+	public String getName(){
+		return name; 
+	}
+	
+	public abstract void setSays(String s);
+	public abstract String getSays();
+	
+	public String toString(){
+		return name + " weighs " + weight + " lbs. and says " + getSays();
+	}
 }
