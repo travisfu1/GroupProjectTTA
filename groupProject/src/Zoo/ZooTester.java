8d8ce7334bc321b9d1dfc451382dc41f7890a208
Travis FU <travis.b.fu@gmail.com>
2016-05-10 23:41:31 -0700

    lkh

--- groupProject/src/Zoo/ZooTester.java
+++ groupProject/src/Zoo/ZooTester.java
@@ -19,14 +19,16 @@ public class ZooTester {
 		Scanner file = new Scanner(new File("src/Zoo/data.dat"));
 
 		Zoo exi = new Zoo(file.nextLine());
-		while (file.hasNextLine())
+		while (file.hasNextLine()) {
 			exi.setZooAverage(file.nextLine());
-
+			exi.setPopulus(file.nextLine());
+		}
 		out.print("The Average Weight of All the Zoo Animals is: ");
 		out.printf("%.2f\n", exi.getZooAverage());
 		out.print("\n");
 
 		// Trying to print out all the animals in the zoo:
+<<<<<<< Updated upstream
 		System.out.println("All of the Beautiful Animals in Our Lovely Zoo: " + "\n");
 		Scanner file2 = new Scanner(new File("src/Zoo/population.dat"));
 
@@ -63,6 +65,10 @@ public class ZooTester {
 			// file2.nextLine();
 
 		} while (file2.hasNextLine());
+=======
+		System.out.println("All of the Beautiful Animals in Our Lovely Zoo: "
+				+ "\n");
+>>>>>>> Stashed changes
 
 	}
 }
